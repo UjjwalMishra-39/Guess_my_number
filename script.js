fb4e412e91053d8d1a4dc29ab4cacdc130125f10
Ujjwal Mishra <158471967+UjjwalMishra-39@users.noreply.github.com>
2024-03-15 12:22:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -77,6 +77,6 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
 
-  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('body').style.backgroundColor = '#00008b';
   document.querySelector('.number').style.width = '15rem';
 });
